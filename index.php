26cd5455fbc99ac8e5cbaff6e2fef1f7a5f8d2c9
helene <helene.papin62@gmail.com>
2017-06-07 17:47:53 +0200

    Change Framework

--- index.php
+++ index.php
@@ -7,40 +7,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Poppins" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/kube.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="style/css/main.css">
 
 
 </head>
 <body>
-    <!-- <nav class="nav row around align-middle" id="navbar" >
-        <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav> -->
-    <section class="home container">
-        <div class="row">
-            <div class="col col-11 socialNetwork push-middle">
-                <a href="http://miinow-hp.deviantart.com/" target="_blank"><i class="fa fa-deviantart fa-2x" aria-hidden="true"></i></a>
-                <a href="https://github.com/miihelene" target="_blank"><i class="fa fa-github-square fa-2x" aria-hidden="true"></i></a>
-                <a href="http://fr.viadeo.com/fr/profile/miinow-helene.papin" target="_blank"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
-                <a href="https://www.linkedin.com/in/h%C3%A9l%C3%A8ne-papin-bb1254119/?ppe=1" target="_blank"><i class="fa fa-viadeo-square fa-2x" aria-hidden="true"></i></a>
+
+    <section class="container home">
+
+        <nav class="navbar">
+          <div class="container-fluid">
+            <div class="navbar-header">
+                <ul>
+                    <li><a href="http://miinow-hp.deviantart.com/" target="_blank"><i class="fa fa-deviantart fa-2x" aria-hidden="true"></i></a></li>
+                    <li><a href="https://github.com/miihelene" target="_blank"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
+                    <li> <a href="http://fr.viadeo.com/fr/profile/miinow-helene.papin" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/h%C3%A9l%C3%A8ne-papin-bb1254119/?ppe=1" target="_blank"><i class="fa fa-viadeo fa-2x" aria-hidden="true"></i></a></li>
+                </ul>
             </div>
-            <div class="col col-1 push-middle">
-                <a href="#" class="btnNav">Menu</a>
+            <div class="collapse navbar-collapse navbar-right">
+                <a href="#" class="btn">Menu</a>
+            </div>
+          </div>
+        </nav>
+
+
+        <div class="row">
+            <div class="col-xs-12 col-md-6">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
         </div>
     </section>
 
 
-    <!-- Kube JS + jQuery are used for some functionality, but are not required for the basic setup -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://use.fontawesome.com/e1876fde46.js"></script>
-    <script src="assets/js/kube.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
