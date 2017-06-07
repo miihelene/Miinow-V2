65059f1123546c7e89363329954fe956f9f28863
helene <helene.papin62@gmail.com>
2017-06-07 12:55:06 +0200

    Top Part (Social Network + Btn Nav) - CLEAR

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Miinow | HP Book</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Poppins" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/kube.css">
+    <link rel="stylesheet" href="style/css/main.css">
+
+
+</head>
+<body>
+    <!-- <nav class="nav row around align-middle" id="navbar" >
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav> -->
+    <section class="home container">
+        <div class="row">
+            <div class="col col-11 socialNetwork push-middle">
+                <a href="http://miinow-hp.deviantart.com/" target="_blank"><i class="fa fa-deviantart fa-2x" aria-hidden="true"></i></a>
+                <a href="https://github.com/miihelene" target="_blank"><i class="fa fa-github-square fa-2x" aria-hidden="true"></i></a>
+                <a href="http://fr.viadeo.com/fr/profile/miinow-helene.papin" target="_blank"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/h%C3%A9l%C3%A8ne-papin-bb1254119/?ppe=1" target="_blank"><i class="fa fa-viadeo-square fa-2x" aria-hidden="true"></i></a>
+            </div>
+            <div class="col col-1 push-middle">
+                <a href="#" class="btnNav">Menu</a>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Kube JS + jQuery are used for some functionality, but are not required for the basic setup -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://use.fontawesome.com/e1876fde46.js"></script>
+    <script src="assets/js/kube.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
